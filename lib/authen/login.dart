c123120bbc7f0a7f3d24ed5c69a6abfe1bace9cc
Nuragafi <rafi.priyamitra.nuraga@gmail.com>
2022-10-21 11:14:27 +0700

    first commit

---
+++ lib/authen/login.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
